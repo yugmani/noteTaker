f1c9a7eca08819cffe6c6f09b4b4c05eaf88731f
Himalaya <yugmani@yahoo.com>
2019-12-13 22:45:38 -0800

    updated

---
+++ app.js
@@ -0,0 +1,74 @@
+// Dependencies
+
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// (DATA)
+
+
+
+// Routes
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// Displays all notes
+app.get("/api/notes", function(req, res) {
+  return res.json("./db/db.json");
+});
+
+
+// Create New Note - takes in JSON input
+app.post("/api/notes", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newNote = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+ 
+  //newNote.routeName = newNote.note.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newNote);
+
+  notes.push(newNote);
+
+  res.json(newNote);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+
+
+// Displays a single character, or returns false
+// app.get("/api/db/:id", function(req, res) {
+//   var chosen = req.params.character;
+
+//   console.log(chosen);
+
+//   for (var i = 0; i < characters.length; i++) {
+//     if (chosen === characters[i].routeName) {
+//       return res.json(characters[i]);
+//     }
+//   }
+
+//   return res.json(false);
+// });
